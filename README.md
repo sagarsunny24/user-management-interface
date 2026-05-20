094131e0133deee3ecbcff1bbc3ca258e6700a22
Sagar Sunny <sagarsunny2403@gmail.com>
2026-05-20 15:23:43 +0530

    fixed formatting

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # User Management Interface
+
 A responsive user management interface built with vanilla HTML, CSS and JavaScript
 
 # Documentation of JavaScript code
@@ -6,15 +7,17 @@ A responsive user management interface built with vanilla HTML, CSS and JavaScri
 ## Defining global scope variables
 
 ```js
-let users = []
-let editingId = null
-let userToRemove = null
+let users = [];
+let editingId = null;
+let userToRemove = null;
 ```
+
 users - an array of objects - each object an user
 editingId - to know which record was chosen for editing
 userToRemove - to hold id of user to remove
 
 ## Element Selectors for HTML
+
 .
 .
 .
@@ -38,15 +41,16 @@ function addUser() {
     firstName: firstNameInp.value.trim(),
     lastName: lastNameInp.value.trim(),
     email: emailInput.value.trim(),
-    mobile: mobileInput.value.trim()
-  }
-  users.push(user)
-  renderUsers()
-  closeFormModal()
+    mobile: mobileInput.value.trim(),
+  };
+  users.push(user);
+  renderUsers();
+  closeFormModal();
 }
 ```
 
 ## Function that validates the user inputs, has both empty checks and regEx checks for names, email, mobile
+
 Browser validation is turned off with `novalidate` in form tag, and I manually did validation using JS
 
 ```js
@@ -83,24 +87,28 @@ function clearErrors()
 ```
 
 ## Function that renders the table and its body - most important
+
 It checks the users array, displays empty if it has no elements
 If non-empty - it maps each object in the array to an innerHTML with data inserted into it, calls `UpdateCount()` to update the number of users in the top right corner
 It also adds the user.id (unique Id) into the buttons dataset `data-id`
+
 ```js
 function renderUsers() {
-  tableBody.innerHTML=""
-  if (users.length == 0){
-    emptyState.style.display="block"
-    tableWrap.classList.remove("is-visible")
-    UpdateCount()
-    return
+  tableBody.innerHTML = "";
+  if (users.length == 0) {
+    emptyState.style.display = "block";
+    tableWrap.classList.remove("is-visible");
+    UpdateCount();
+    return;
   }
 
-  emptyState.style.display = "none"
-  tableWrap.classList.add('is-visible')
-  userCount.textContent= users.length + " users"
+  emptyState.style.display = "none";
+  tableWrap.classList.add("is-visible");
+  userCount.textContent = users.length + " users";
 
-  tableBody.innerHTML = users.map(user => `
+  tableBody.innerHTML = users
+    .map(
+      (user) => `
     <tr>
       <td> ${user.firstName} ${user.lastName}</td>
       <td> ${user.email}</td>
@@ -109,8 +117,10 @@ function renderUsers() {
         <button class="btn-edit" data-id="${user.id}">Edit</button>
         <button class="btn-remove" data-id="${user.id}">Remove</button>
       </td>
-    </tr>`).join("");
-  UpdateCount()
+    </tr>`,
+    )
+    .join("");
+  UpdateCount();
 }
 ```
 
@@ -122,56 +132,61 @@ function openConfirmModal()
 function closeConfirmModal()
 function closeFormModal()
 ```
+
 ## Function when we press the Edit button for a record
 
 First checks if there is user of specific id is present, then , populates the field with values from the user object of users array
 This function is called via event delegation done to the Edit button in the table, if the edit button is pressed it does 2 things - sets the editingId = id - which is later used to check when the form is submitted - if editingId is null - it is new user, if not null it is updating record of a user
 
 ```js
-function openEditModal(id){
-  const user = users.find(function(u) { return u.id === id})
-  if(!user) return
-  editingId = id
-  modalEyebrow.textContent= "Edit Record"
-  modalTitle.textContent = "Edit User"
-  btnSubmit.textContent = "Update User"
-  firstNameInp.value = user.firstName
-  lastNameInp.value = user.lastName
-  emailInput.value = user.email
-  mobileInput.value = user.mobile
-  clearErrors()
-  formOverlay.classList.add('is-open')
-  firstNameInp.focus()
+function openEditModal(id) {
+  const user = users.find(function (u) {
+    return u.id === id;
+  });
+  if (!user) return;
+  editingId = id;
+  modalEyebrow.textContent = "Edit Record";
+  modalTitle.textContent = "Edit User";
+  btnSubmit.textContent = "Update User";
+  firstNameInp.value = user.firstName;
+  lastNameInp.value = user.lastName;
+  emailInput.value = user.email;
+  mobileInput.value = user.mobile;
+  clearErrors();
+  formOverlay.classList.add("is-open");
+  firstNameInp.focus();
 }
 ```
 
-## Function that updates the users array 
+## Function that updates the users array
+
 After updating calls the `renderUsers()` to reflect the change
 
 ```js
-function updateUser(){
-  for(let i = 0;i <users.length; i++){
-    if(users[i].id === editingId) {
-      users[i].firstName = firstNameInp.value.trim()
-      users[i].lastName = lastNameInp.value.trim()
-      users[i].email = emailInput.value.trim()
-      users[i].mobile = mobileInput.value.trim()
-      break
+function updateUser() {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === editingId) {
+      users[i].firstName = firstNameInp.value.trim();
+      users[i].lastName = lastNameInp.value.trim();
+      users[i].email = emailInput.value.trim();
+      users[i].mobile = mobileInput.value.trim();
+      break;
     }
   }
-  renderUsers()
-  closeFormModal()
+  renderUsers();
+  closeFormModal();
 }
 ```
 
 ## Function that removes the user from the users array
+
 This removes the user and again calls `renderUsers()` to reflect the change
 
 ```js
-function removeUser(){
-  users = users.filter(user => user.id !== userToRemove)
-  closeConfirmModal()
-  renderUsers()
+function removeUser() {
+  users = users.filter((user) => user.id !== userToRemove);
+  closeConfirmModal();
+  renderUsers();
 }
 ```
 
@@ -179,8 +194,8 @@ function removeUser(){
 
 ```js
 function UpdateCount() {
-  userCount.textContent = `${users.length} users`
-  return
+  userCount.textContent = `${users.length} users`;
+  return;
 }
 ```
 
@@ -189,54 +204,58 @@ function UpdateCount() {
 These listen for the buttons Add User, Close X, Cancel button, Remove button
 
 ```js
-btnConfirmCancel.addEventListener("click", closeConfirmModal)
-btnConfirmRemove.addEventListener("click",removeUser)
-addBtn.addEventListener("click",openFormModal)
-btnClose.addEventListener("click",closeFormModal)
-btnCancel.addEventListener("click",closeFormModal)
+btnConfirmCancel.addEventListener("click", closeConfirmModal);
+btnConfirmRemove.addEventListener("click", removeUser);
+addBtn.addEventListener("click", openFormModal);
+btnClose.addEventListener("click", closeFormModal);
+btnCancel.addEventListener("click", closeFormModal);
 ```
+
 ## User Form listener
+
 1. it prevents browser from defaulting
 2. validates the form, for both editing and new users
 3. if it was called after the `function openEditModal(id)` was called meanining the `Edit` button was pressed - then the `editingId` variable will have a unique ID, and calls the `updateUser()` function
 4. Else calls the `addUser()` function
 
 ```js
-userForm.addEventListener("submit",function(e){
-  e.preventDefault()
-  if (!validateForm()) return
-  if(editingId) {
-    updateUser()
+userForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!validateForm()) return;
+  if (editingId) {
+    updateUser();
+  } else {
+    addUser();
   }
-  else {
-    addUser()
-  }
-})
+});
 ```
 
 ## Table body listener
+
 when clicking on Edit or Remove button, effectively event is delegated to the table body.
+
 1. Collects the associated user.id previously stored in the DOM for the buttons - This id is used for knowing which record to Edit or Remove
 2. Accordingly function is called
 
 ```js
-tableBody.addEventListener("click", function(e) {
+tableBody.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
-  console.log(id)
-  if(e.target.classList.contains("btn-edit")){
-    openEditModal(id)
+  console.log(id);
+  if (e.target.classList.contains("btn-edit")) {
+    openEditModal(id);
   }
-  if(e.target.classList.contains("btn-remove")){
-    openConfirmModal(id)
+  if (e.target.classList.contains("btn-remove")) {
+    openConfirmModal(id);
   }
-})
+});
 ```
 
 ## Listener for escape key which closes modals
+
 ```js
-document.addEventListener("keydown",(e)=> {
-  if(e.key !=="Escape") return
-  closeFormModal()
-  closeConfirmModal()
-})
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "Escape") return;
+  closeFormModal();
+  closeConfirmModal();
+});
 ```
--- script.js
+++ script.js
@@ -1,126 +1,123 @@
-let users = []
-let editingId = null
-let userToRemove = null
+let users = [];
+let editingId = null;
+let userToRemove = null;
 
-
-const addBtn = document.querySelector("#btn-addnew")
-const formOverlay = document.getElementById('formOverlay')
-const btnClose = document.querySelector('.btn-close')
-const btnCancel = document.getElementById("btnCancel")
-const tableBody = document.getElementById("tableBody")
-const emptyState = document.getElementById('emptyState')
-const tableWrap = document.getElementById('table-wrap')
-const userCount = document.getElementById('userCount')
-const firstNameInp= document.getElementById('firstName')
-const lastNameInp = document.getElementById('lastName')
-const emailInput = document.getElementById('email')
-const mobileInput = document.getElementById('mobile')
-const modalEyebrow = document.getElementById('modalEyebrow')
-const modalTitle = document.getElementById('modalTitle')
-const btnSubmit = document.getElementById('btnSubmit')
-const userForm = document.getElementById('userForm')
-const firstNameErr = document.getElementById('firstNameErr')
-const lastNameErr = document.getElementById('lastNameErr')
-const emailErr = document.getElementById('emailerr')
-const mobileErr = document.getElementById('mobileErr')
-const confirmOverlay = document.getElementById('confirmOverlay')
-const btnConfirmCancel = document.getElementById('btnConfirmCancel')
-const btnConfirmRemove = document.getElementById('btnConfirmRemove')
-
+const addBtn = document.querySelector("#btn-addnew");
+const formOverlay = document.getElementById("formOverlay");
+const btnClose = document.querySelector(".btn-close");
+const btnCancel = document.getElementById("btnCancel");
+const tableBody = document.getElementById("tableBody");
+const emptyState = document.getElementById("emptyState");
+const tableWrap = document.getElementById("table-wrap");
+const userCount = document.getElementById("userCount");
+const firstNameInp = document.getElementById("firstName");
+const lastNameInp = document.getElementById("lastName");
+const emailInput = document.getElementById("email");
+const mobileInput = document.getElementById("mobile");
+const modalEyebrow = document.getElementById("modalEyebrow");
+const modalTitle = document.getElementById("modalTitle");
+const btnSubmit = document.getElementById("btnSubmit");
+const userForm = document.getElementById("userForm");
+const firstNameErr = document.getElementById("firstNameErr");
+const lastNameErr = document.getElementById("lastNameErr");
+const emailErr = document.getElementById("emailerr");
+const mobileErr = document.getElementById("mobileErr");
+const confirmOverlay = document.getElementById("confirmOverlay");
+const btnConfirmCancel = document.getElementById("btnConfirmCancel");
+const btnConfirmRemove = document.getElementById("btnConfirmRemove");
 
 function generateUserId() {
   const id = Date.now().toString(36);
   return id;
 }
 
-function addUser() {  const user = {
+function addUser() {
+  const user = {
     id: generateUserId(),
     firstName: firstNameInp.value.trim(),
     lastName: lastNameInp.value.trim(),
     email: emailInput.value.trim(),
-    mobile: mobileInput.value.trim()
-  }
-  users.push(user)
-  renderUsers()
-  closeFormModal()
+    mobile: mobileInput.value.trim(),
+  };
+  users.push(user);
+  renderUsers();
+  closeFormModal();
 }
-function validateForm(){
-  clearErrors()
-  const firstName = firstNameInp.value.trim()
-  const lastName = lastNameInp.value.trim()
-  const email = emailInput.value.trim()
-  const mobile = mobileInput.value.trim()
-  let isValid = true
+function validateForm() {
+  clearErrors();
+  const firstName = firstNameInp.value.trim();
+  const lastName = lastNameInp.value.trim();
+  const email = emailInput.value.trim();
+  const mobile = mobileInput.value.trim();
+  let isValid = true;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const mobileRegex = /^[0-9]{10}$/;
 
-  if(firstName == ""){
-    firstNameErr.textContent="First name is required";
-    firstNameInp.classList.add("is-error")
-    isValid = false
+  if (firstName == "") {
+    firstNameErr.textContent = "First name is required";
+    firstNameInp.classList.add("is-error");
+    isValid = false;
+  } else if (/\d/.test(firstName)) {
+    firstNameErr.textContent = "First name cannot contain numbers";
+    firstNameInp.classList.add("is-error");
+    isValid = false;
   }
-  else if ( /\d/.test(firstName)) {
-    firstNameErr.textContent= "First name cannot contain numbers";
-    firstNameInp.classList.add("is-error")
-    isValid = false
+  if (lastName == "") {
+    lastNameErr.textContent = "Last name is required";
+    lastNameInp.classList.add("is-error");
+    isValid = false;
+  } else if (/\d/.test(lastName)) {
+    lastNameErr.textContent = "Last name cannot contain numbers";
+    lastNameInp.classList.add("is-error");
+    isValid = false;
   }
-  if(lastName==""){
-    lastNameErr.textContent= "Last name is required";
-    lastNameInp.classList.add("is-error")
-    isValid = false
+  if (email == "") {
+    emailErr.textContent = "Email is required";
+    emailInput.classList.add("is-error");
+    isValid = false;
+  } else if (!emailRegex.test(email)) {
+    emailErr.textContent = "Please enter a valid email";
+    emailInput.classList.add("is-error");
+    isValid = false;
   }
-  else if (/\d/.test(lastName)) {
-    lastNameErr.textContent="Last name cannot contain numbers";
-    lastNameInp.classList.add("is-error")
-    isValid = false
+  if (mobile == "") {
+    mobileErr.textContent = "Mobile number is required";
+    mobileInput.classList.add("is-error");
+    isValid = false;
+  } else if (!mobileRegex.test(mobile)) {
+    mobileErr.textContent = "Enter a valid 10-digit number";
+    mobileInput.classList.add("is-error");
+    isValid = false;
   }
-  if(email ==""){
-    emailErr.textContent="Email is required";
-    emailInput.classList.add("is-error")
-    isValid = false
-  }
-  else if(!emailRegex.test(email)){
-    emailErr.textContent="Please enter a valid email";
-    emailInput.classList.add("is-error")
-    isValid = false
-  }
-  if(mobile==""){
-    mobileErr.textContent = "Mobile number is required"
-    mobileInput.classList.add("is-error")
-    isValid = false
-  }
-  else if (!mobileRegex.test(mobile)){
-    mobileErr.textContent = "Enter a valid 10-digit number"
-    mobileInput.classList.add("is-error")
-    isValid = false
-  }
-  return isValid
+  return isValid;
 }
 
-function clearErrors(){
-      firstNameErr.textContent=""
-      lastNameErr.textContent=""
-      emailErr.textContent=""
-      mobileErr.textContent=""
-      firstNameInp.classList.remove("is-error")
-      lastNameInp.classList.remove("is-error")
-      emailInput.classList.remove("is-error")
-      mobileInput.classList.remove("is-error")
+function clearErrors() {
+  firstNameErr.textContent = "";
+  lastNameErr.textContent = "";
+  emailErr.textContent = "";
+  mobileErr.textContent = "";
+  firstNameInp.classList.remove("is-error");
+  lastNameInp.classList.remove("is-error");
+  emailInput.classList.remove("is-error");
+  mobileInput.classList.remove("is-error");
 }
 function renderUsers() {
-  tableBody.innerHTML=""
-  if (users.length == 0){
-    emptyState.style.display="block"
-    tableWrap.classList.remove("is-visible")
-    UpdateCount()
-    return
+  tableBody.innerHTML = "";
+  if (users.length == 0) {
+    emptyState.style.display = "block";
+    tableWrap.classList.remove("is-visible");
+    UpdateCount();
+    return;
   }
 
-  emptyState.style.display = "none"
-  tableWrap.classList.add('is-visible')
-  userCount.textContent= users.length + " users"
+  emptyState.style.display = "none";
+  tableWrap.classList.add("is-visible");
+  userCount.textContent = users.length + " users";
 
-  tableBody.innerHTML = users.map(user => `
+  tableBody.innerHTML = users
+    .map(
+      (user) => `
     <tr>
       <td> ${user.firstName} ${user.lastName}</td>
       <td> ${user.email}</td>
@@ -129,112 +126,108 @@ function renderUsers() {
         <button class="btn-edit" data-id="${user.id}">Edit</button>
         <button class="btn-remove" data-id="${user.id}">Remove</button>
       </td>
-    </tr>`).join("");
-  UpdateCount()
+    </tr>`,
+    )
+    .join("");
+  UpdateCount();
 }
 
-
-
 function openFormModal() {
-  editingId = null
+  editingId = null;
   modalEyebrow.textContent = "New Record";
-  modalTitle.textContent = "Add User"
-  btnSubmit.textContent = "Save User"
-  userForm.reset()
-  formOverlay.classList.add('is-open');
-  firstNameInp.focus()
-
+  modalTitle.textContent = "Add User";
+  btnSubmit.textContent = "Save User";
+  userForm.reset();
+  formOverlay.classList.add("is-open");
+  firstNameInp.focus();
 }
 
 function openConfirmModal(id) {
-  userToRemove = id
-  confirmOverlay.classList.add('is-open')
-
+  userToRemove = id;
+  confirmOverlay.classList.add("is-open");
 }
 function closeConfirmModal() {
-  confirmOverlay.classList.remove("is-open")
-  userToRemove = null
+  confirmOverlay.classList.remove("is-open");
+  userToRemove = null;
 }
-function closeFormModal(){
-  editingId = null
-  addBtn.focus()
-  clearErrors()
-  userForm.reset()
-  formOverlay.classList.remove('is-open');
-}
-
-function openEditModal(id){
-  const user = users.find(function(u) { return u.id === id})
-  if(!user) return
-  editingId = id
-  modalEyebrow.textContent= "Edit Record"
-  modalTitle.textContent = "Edit User"
-  btnSubmit.textContent = "Update User"
-  firstNameInp.value = user.firstName
-  lastNameInp.value = user.lastName
-  emailInput.value = user.email
-  mobileInput.value = user.mobile
-  clearErrors()
-  formOverlay.classList.add('is-open')
-  firstNameInp.focus()
+function closeFormModal() {
+  editingId = null;
+  addBtn.focus();
+  clearErrors();
+  userForm.reset();
+  formOverlay.classList.remove("is-open");
 }
 
-function updateUser(){
-  for(let i = 0;i <users.length; i++){
-    if(users[i].id === editingId) {
-      users[i].firstName = firstNameInp.value.trim()
-      users[i].lastName = lastNameInp.value.trim()
-      users[i].email = emailInput.value.trim()
-      users[i].mobile = mobileInput.value.trim()
-      break
+function openEditModal(id) {
+  const user = users.find(function (u) {
+    return u.id === id;
+  });
+  if (!user) return;
+  editingId = id;
+  modalEyebrow.textContent = "Edit Record";
+  modalTitle.textContent = "Edit User";
+  btnSubmit.textContent = "Update User";
+  firstNameInp.value = user.firstName;
+  lastNameInp.value = user.lastName;
+  emailInput.value = user.email;
+  mobileInput.value = user.mobile;
+  clearErrors();
+  formOverlay.classList.add("is-open");
+  firstNameInp.focus();
+}
+
+function updateUser() {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === editingId) {
+      users[i].firstName = firstNameInp.value.trim();
+      users[i].lastName = lastNameInp.value.trim();
+      users[i].email = emailInput.value.trim();
+      users[i].mobile = mobileInput.value.trim();
+      break;
     }
   }
-  renderUsers()
-  closeFormModal()
+  renderUsers();
+  closeFormModal();
 }
 
-
-function removeUser(){
-  users = users.filter(user => user.id !== userToRemove)
-  closeConfirmModal()
-  renderUsers()
+function removeUser() {
+  users = users.filter((user) => user.id !== userToRemove);
+  closeConfirmModal();
+  renderUsers();
 }
 
 function UpdateCount() {
-  userCount.textContent = `${users.length} users`
-  return
+  userCount.textContent = `${users.length} users`;
+  return;
 }
 
-addBtn.addEventListener("click",openFormModal)
-btnClose.addEventListener("click",closeFormModal)
-btnCancel.addEventListener("click",closeFormModal)
-btnConfirmCancel.addEventListener("click", closeConfirmModal)
-btnConfirmRemove.addEventListener("click",removeUser)
-userForm.addEventListener("submit",function(e){
-  e.preventDefault()
-  if (!validateForm()) return
-  if(editingId) {
-    updateUser()
+addBtn.addEventListener("click", openFormModal);
+btnClose.addEventListener("click", closeFormModal);
+btnCancel.addEventListener("click", closeFormModal);
+btnConfirmCancel.addEventListener("click", closeConfirmModal);
+btnConfirmRemove.addEventListener("click", removeUser);
+userForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!validateForm()) return;
+  if (editingId) {
+    updateUser();
+  } else {
+    addUser();
   }
-  else {
-    addUser()
-  }
-})
+});
 
-tableBody.addEventListener("click", function(e) {
+tableBody.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
-  console.log(id)
-  if(e.target.classList.contains("btn-edit")){
-    openEditModal(id)
+  console.log(id);
+  if (e.target.classList.contains("btn-edit")) {
+    openEditModal(id);
   }
-  if(e.target.classList.contains("btn-remove")){
-    openConfirmModal(id)
+  if (e.target.classList.contains("btn-remove")) {
+    openConfirmModal(id);
   }
-})
-document.addEventListener("keydown",(e)=> {
-  if(e.key !=="Escape") return
-  closeFormModal()
-  closeConfirmModal()
-})
-
-
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "Escape") return;
+  closeFormModal();
+  closeConfirmModal();
+});
